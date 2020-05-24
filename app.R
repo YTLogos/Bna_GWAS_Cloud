46e0343638976d4646d827f8dc17cfe5bcbb079b
YTLogos <tyan@zju.edu.cm>
2020-05-24 12:31:23 +0800

    addd zju link

--- app.R
+++ app.R
@@ -50,7 +50,7 @@ sidebar <- dashboardSidebar(
     height = "auto",
     div(span("Developed by", style = "color:grey"), a("JiangLX Lab", href = "http://rapeseed.zju.edu.cn", target = "_blank"), span(a(", College of", href = "http://www.cab.zju.edu.cn/en/", target = "_blank"), style = "color:grey")),
     div(a("Agriculture and Biotechnology (CAB),", href = "http://www.cab.zju.edu.cn/en/", target = "_blank"), style = "color:grey"),
-    div("Zhejiang University", style = "color:grey")
+    div(a("Zhejiang University,",href="http://www.zju.edu.cn/english/",target="_blank"), style = "color:grey")
   )
 )
 
